ce1b03bab3dcc1d6685d942aaa64d64e0082db11
BoyJn <32192170+BoyJJ@users.noreply.github.com>
2020-01-18 11:36:38 +0800

    Revert "Revert "AddpartofWT""
    This reverts commit 5b175cb4c2ea63d2b255998795590f4401411df7.

---
+++ SchoolOA/Controllers/FinanceController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolOA.Controllers
+{
+    public class FinanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PersonalWage()
+        {
+            return View();
+        }
+
+        public IActionResult SectionWage()
+        {
+            return View();
+        }
+
+        public IActionResult EditWage(string teacherid, string time)
+        {
+            ViewData["teacherid"] = teacherid;
+            ViewData["time"] = time;
+            return View();
+        }
+
+
+        public IActionResult MaterialTable()
+        {
+            return View();
+        }
+
+        public IActionResult MaterialDetail(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+    }
+}
